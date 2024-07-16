0782801cba9a043bb6f79ef1eab4f2c68b3c23ba
Teera Tesharojanasup <teerareal74@gmail.com>
2024-07-16 17:35:17 -0400

    Programming Assignment 1 Template

---
+++ Programming_Assignments/Assignment_1/hw1.py
@@ -0,0 +1,95 @@
+import time
+import numpy as np
+from gridgame import *
+
+##############################################################################################################################
+
+# You can visualize what your code is doing by setting the GUI argument in the following line to true.
+# The render_delay_sec argument allows you to slow down the animation, to be able to see each step more clearly.
+
+# For your final submission, please set the GUI option to False.
+
+# The gs argument controls the grid size. You should experiment with various sizes to ensure your code generalizes.
+
+##############################################################################################################################
+
+setup(GUI = False, render_delay_sec = 0.1, gs = 7)
+
+
+##############################################################################################################################
+
+# Initialization
+
+# grid represents the current state of the board. 
+    
+    # -1 indicates an empty cell
+    # 0 indicates a cell colored in the first color (indigo by default)
+    # 1 indicates a cell colored in the second color (taupe by default)
+    # 2 indicates a cell colored in the third color (veridian by default)
+    # 3 indicates a cell colored in the fourth color (peach by default)
+
+# placedShapes is a list of shapes that have currently been placed on the board.
+    
+    # Each shape is represented as a list containing three elements: a) the brush type (number between 0-8), 
+    # b) the location of the shape (coordinates of top-left cell of the shape) and c) color of the shape (number between 0-3)
+
+    # For instance [0, (0,0), 2] represents a shape spanning a single cell in the color 2=veridian, placed at the top left cell in the grid.
+
+# done is a Boolean that represents whether coloring constraints are satisfied. Updated by the gridgames.py file.
+
+##############################################################################################################################
+
+grid, placedShapes, done = execute('export')
+#input()   # <-- workaround to prevent PyGame window from closing after execute() is called, for when GUI set to True. Uncomment to enable.
+print(grid, placedShapes, done)
+
+
+
+####################################################
+# Timing your code's execution for the leaderboard.
+####################################################
+
+start = time.time()  # <- do not modify this.
+
+
+
+##########################################
+# Write all your code in the area below. 
+##########################################
+
+
+
+
+
+
+
+
+'''
+
+YOUR CODE HERE
+
+
+'''
+
+
+
+
+
+
+
+
+
+
+########################################
+
+# Do not modify any of the code below. 
+
+########################################
+
+end=time.time()
+
+np.savetxt('grid.txt', grid, fmt="%d")
+with open("shapes.txt", "w") as outfile:
+    outfile.write(str(placedShapes))
+with open("time.txt", "w") as outfile:
+    outfile.write(str(end-start))
