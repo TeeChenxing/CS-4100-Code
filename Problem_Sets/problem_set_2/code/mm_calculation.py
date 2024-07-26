5e7c82e15dfb95401454f84a865d18bbf5fa2c40
Teera Tesharojanasup <teerareal74@gmail.com>
2024-07-26 18:46:44 -0400

    PA2 90% Completed

--- Problem_Sets/problem_set_2/code/mm_calculation.py
+++ Problem_Sets/problem_set_2/code/mm_calculation.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 
-def compute_stationary_distribution(A, p):
-    for _ in range(10000):
+def compute_stationary_distribution(A, p, n_times):
+    for _ in range(n_times):
         p = p.dot(A)
     return p
 
@@ -13,7 +13,7 @@ def main():
                                [0.33, 0.33, 0.34]])
 
     initial_distribution = np.array([0.8, 0.1, 0.1])
-    stationary_distribution = compute_stationary_distribution(weather_matrix, initial_distribution)
+    stationary_distribution = compute_stationary_distribution(weather_matrix, initial_distribution, 10000)
     print(f"Weather Matrix Stationary Distribution: {stationary_distribution}")
 
     Q1 = np.array([[0.1, 0.9, 0.0], 
@@ -21,7 +21,7 @@ def main():
                    [0.7, 0.3, 0.0]])
 
     p0 = np.array([0.1, 0.6, 0.3])
-    stationary_distribution = compute_stationary_distribution(Q1, p0)
+    stationary_distribution = compute_stationary_distribution(Q1, p0, 10000)
     print(f"Problem Set Matrix: {stationary_distribution}")
 
     q4_value = (0.409 * 0.5 * 0.9) + (0.409 * 0 * 0) + (0.409 * 0.5 * 0.3)
@@ -30,6 +30,10 @@ def main():
     q5_value = (0.1 * 0.1 * 0.9) + (0.1 * 0.9 * 0) + (0.1 * 0 * 0.3) + (0.6 * 0.5 * 0.9) + (0.6 * 0 * 0) + (0.6 * 0.5 * 0.3) + (0.3 * 0.7 * 0.9) + (0.3 * 0.3 * 0) + (0.3 * 0 * 0.3)
     print(f"Q5 Value: {q5_value}")
 
+    #################################
+
+    X_Y_S2 = compute_stationary_distribution(Q1, p0, 2)
+    print(f"Q5 X --> Y --> 2: {X_Y_S2}")
 
     #################################
 
@@ -37,7 +41,7 @@ def main():
                             [0.34, 0.66]])
 
     initial_distribution = np.array([0.5, 0.5])
-    stationary_distribution = compute_stationary_distribution(coin_matrix, initial_distribution)
+    stationary_distribution = compute_stationary_distribution(coin_matrix, initial_distribution, 10000)
     print(f"Coin Matrix Stationary Distribution: {stationary_distribution}")
 
 if __name__ == "__main__":
